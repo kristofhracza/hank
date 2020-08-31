035589ae00051ab4223e76583dd313a248ce1670
Kristof Hracza <57770911+IVBecy@users.noreply.github.com>
2020-08-31 23:25:09 +0100

    added formatting to sites

--- spy.py
+++ spy.py
@@ -2,7 +2,6 @@
 import requests
 import sys
 from optparse import OptionParser
-from colorama import Fore, Style, init
 
 # method to be called when the user asks for help
 def usage():
@@ -42,31 +41,31 @@ if options.help:
 ##### Variables
 # Sites
 sites = {
-  "AboutMe": "https://about.me/",
-  "Badoo": "https://badoo.com/",
-  "CodePen": "https://codepen.io/",
-  "DevCommunity": "https://dev.to/",
-  "Discogs": "https://www.discogs.com/user/",
-  "Ello": "https://ello.co/",
-  "Github": "https://github.com/",
-  "IFTT": "https://www.ifttt.com/p/",
-  "Instagram": "https://www.instagram.com/",
-  "Keybase": "https://keybase.io/",
-  "MySpace": "https://myspace.com/",
-  "PasteBin": "https://pastebin.com/u/",
-  "Patreon": "https://www.patreon.com/",
-  "PCPartPicker": "https://pcpartpicker.com/user/",
-  "Pinterest": "https://www.pinterest.com/",
-  "Reddit": "https://www.reddit.com/user/",
-  "SoundCloud": "https://soundcloud.com/",
-  "Spotify": "https://open.spotify.com/user/",
-  "Tellonym": "https://tellonym.me/",
-  "TryHackMe": "https://tryhackme.com/p/",
-  "Twitch": "https://m.twitch.tv/",
-  "Twitter": "https://mobile.twitter.com/",
-  "Wordpress": "https://profiles.wordpress.org/",
-  "Xbox": "https://xboxgamertag.com/search/",
-  "Youtube": "https://www.youtube.com/",
+  "AboutMe": "https://about.me/{}",
+  "Badoo": "https://badoo.com/{}",
+  "CodePen": "https://codepen.io/{}",
+  "DevCommunity": "https://dev.to/{}",
+  "Discogs": "https://www.discogs.com/user/{}",
+  "Ello": "https://ello.co/{}",
+  "Github": "https://github.com/{}",
+  "IFTTT": "https://www.ifttt.com/p/{}",
+  "Instagram": "https://www.instagram.com/{}",
+  "Keybase": "https://keybase.io/{}",
+  "MySpace": "https://myspace.com/{}",
+  "PasteBin": "https://pastebin.com/u/{}",
+  "Patreon": "https://www.patreon.com/{}",
+  "PCPartPicker": "https://pcpartpicker.com/user/{}",
+  "Pinterest": "https://www.pinterest.com/{}",
+  "Reddit": "https://www.reddit.com/user/{}",
+  "SoundCloud": "https://soundcloud.com/{}",
+  "Spotify": "https://open.spotify.com/user/{}",
+  "Tellonym": "https://tellonym.me/{}",
+  "TryHackMe": "https://tryhackme.com/p/{}",
+  "Twitch": "https://m.twitch.tv/{}",
+  "Twitter": "https://mobile.twitter.com/{}",
+  "Wordpress": "https://profiles.wordpress.org/{}",
+  "Xbox": "https://xboxgamertag.com/search/{}",
+  "Youtube": "https://www.youtube.com/{}",
 }
 
 # Headers 
@@ -91,7 +90,7 @@ print("\n")
 
 #### Appending username to all sites + checking
 for i in sites:
-  sites[i] = sites[i] + options.uname
+  sites[i] = sites[i].format(options.uname)
   if i == "Twitter":
     req = requests.get(sites[i])
   else:
