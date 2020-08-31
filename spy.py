e2a858cb7fb56440b14ec8ef12d2d8084d203c18
Kristof Hracza <57770911+IVBecy@users.noreply.github.com>
2020-08-31 20:20:02 +0100

    Updated site list +added optparser

--- spy.py
+++ spy.py
@@ -1,41 +1,72 @@
-# User name lookup by IVBecy
 ##### Getting module(s)
 import requests
-import time
 import sys
+from optparse import OptionParser
+from colorama import Fore, Style, init
 
-## Help
-if sys.argv[1] == "-h":
+# method to be called when the user asks for help
+def usage():
   print("""
-    Usage: spy.py [username]
-    """)
+  
+    Usage: spy.py  -u [username] -f [file name]
+
+    REQUIRED:
+      -u or --username: Username of the victim
+
+    OPTIONAL:
+      -f or --file: the file to output the results into
+
+  """)
   sys.exit()
-# Input
-uname = sys.argv[1]
+
+#Setting up the options for the terminal
+parser = OptionParser()
+parser.set_conflict_handler("resolve")
+parser.add_option("-u", "--username",dest="uname")
+parser.add_option("-h", "--help", dest="help", action="store_true")
+parser.add_option("-f", "--file", dest="fileName")
+(options, args) = parser.parse_args()
+
+# If the username is set, the help menu cannot be shown (if called at the same time)
+if options.uname:
+  options.help = None
+
+# Run the help menu
+### If the username is not defined
+if options.uname == None:
+  usage()
+### If the user has asked for help
+if options.help:
+  usage()
 
 ##### Variables
 # Sites
 sites = {
-    "Reddit": "https://www.reddit.com/user/",
-    "Github": "https://github.com/",
-    "Facebook": "https://www.facebook.com/",
-    "Instagram": "https://www.instagram.com/",
-    "Wordpress": "https://profiles.wordpress.org/",
-    "Xbox": "https://xboxgamertag.com/search/",
-    "Badoo": "https://badoo.com/",
-    "Pinterest": "https://www.pinterest.com/",
-    "SoundCloud": "https://soundcloud.com/",
-    "TryHackMe": "https://tryhackme.com/p/",
-    "Patreon": "https://www.patreon.com/",
-    "CodePen": "https://codepen.io/",
-    "PasteBin":"https://pastebin.com/u/",
-    "Spotify":"https://open.spotify.com/user/",
-    "Tellonym": "https://tellonym.me/",
-    "Youtube": "https://www.youtube.com/",
-    "AboutMe": "https://about.me/",
-    "IFTT": "https://www.ifttt.com/p/",
-    "MySpace": "https://myspace.com/",
-    "PCPartPicker": "https://pcpartpicker.com/user/"
+  "AboutMe": "https://about.me/",
+  "Badoo": "https://badoo.com/",
+  "CodePen": "https://codepen.io/",
+  "DevCommunity": "https://dev.to/",
+  "Discogs": "https://www.discogs.com/user/",
+  "Ello": "https://ello.co/",
+  "Github": "https://github.com/",
+  "IFTT": "https://www.ifttt.com/p/",
+  "Instagram": "https://www.instagram.com/",
+  "Keybase": "https://keybase.io/",
+  "MySpace": "https://myspace.com/",
+  "PasteBin": "https://pastebin.com/u/",
+  "Patreon": "https://www.patreon.com/",
+  "PCPartPicker": "https://pcpartpicker.com/user/",
+  "Pinterest": "https://www.pinterest.com/",
+  "Reddit": "https://www.reddit.com/user/",
+  "SoundCloud": "https://soundcloud.com/",
+  "Spotify": "https://open.spotify.com/user/",
+  "Tellonym": "https://tellonym.me/",
+  "TryHackMe": "https://tryhackme.com/p/",
+  "Twitch": "https://m.twitch.tv/",
+  "Twitter": "https://mobile.twitter.com/",
+  "Wordpress": "https://profiles.wordpress.org/",
+  "Xbox": "https://xboxgamertag.com/search/",
+  "Youtube": "https://www.youtube.com/",
 }
 
 # Headers 
@@ -45,19 +76,36 @@ headers = {
 
 # Info
 print("\n")
-print("Username: " + uname)
+print("""
+  ===================================
+  ||    ----USERNAME LOOKUP----    ||
+  ||-------------------------------||
+  ||        Creator: IVBecy        ||
+  ||-------------------------------||
+  || Inspired by sherlock-project  ||
+  ||-------------------------------||
+  ===================================
+""")
+print(f"Username: {options.uname}")
 print("\n")
 
 #### Appending username to all sites + checking
 for i in sites:
-  sites[i] = sites[i] + uname
-  req = requests.get(sites[i], headers=headers)
+  sites[i] = sites[i] + options.uname
+  if i == "Twitter":
+    req = requests.get(sites[i])
+  else:
+    req = requests.get(sites[i], headers=headers)
   if req.status_code == 200:
     print(f"[+] {i}: {sites[i]}")
+    # If we need to write to a file
+    if options.fileName:
+      f = open(options.fileName, "a")
+      f.write(f"{i}: {sites[i]}")
+      f.write("\n")
   elif req.status_code == 404:
     print(f"[-] {i}: Not Found")
   else:
-    print(f"[-] {i}: Error: {req.status_code}")
- 
+    print(f"[-] {i}: Error: {req.status_code} {req.reason}")
 
 
