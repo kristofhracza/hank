2020187f821dfeee8aeb6805406e824fa2cc658d
IVBecy <kristofhracza@gmail.com>
2024-01-22 22:01:49 +0000

    ignore pycache dirs

--- lib/setup.py
+++ lib/setup.py
@@ -22,7 +22,7 @@ UA = [
     "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
 ]
 
- # Help
+ # Helps
 def usage():
     print("""
         Usage: bino.py  -u [username] -f [file name]
